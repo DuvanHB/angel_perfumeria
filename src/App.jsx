9b896d17c39a9dc7f63cabf32ccd59f18e1f1ae5
DuvanHB <78061522+DuvanHB@users.noreply.github.com>
2025-09-13 17:24:06 -0500

    extra improvements

--- src/App.jsx
+++ src/App.jsx
@@ -204,7 +204,8 @@ function App() {
             justifyContent: "space-between", 
             fontSize: "1.5rem", 
             fontWeight: "bold", 
-            padding: "1rem" 
+            padding: "1rem",
+            borderBottom: "1px solid rgb(221, 221, 221)"
           }}>
           <span> Filtros</span>
           <img
@@ -214,15 +215,17 @@ function App() {
             onClick={() => {
               setFilter("all");
               setSelectedMarca("all");
+              setSelectedCantidad("all");
               setSearch("");
             }}
           />
           </div>
+          <div style={{fontSize: "1.2rem", fontWeight: "bold", padding: "1rem"}}>Género</div>
           <div
             style={{
               display: "flex",
               justifyContent: "space-around",
-              marginBottom: "20px",
+              marginBottom: "5px",
             }}
           >
             <button
@@ -264,6 +267,7 @@ function App() {
             
           </div>
 
+          <div style={{fontSize: "1.2rem", fontWeight: "bold", padding: "0 1rem 0.5rem"}}>Marca</div>
           <select
             value={selectedMarca}
             onChange={e => setSelectedMarca(e.target.value)}
@@ -281,6 +285,8 @@ function App() {
               <option key={idx} value={marca}>{marca}</option>
             ))}
           </select>
+
+          <div style={{fontSize: "1.2rem", fontWeight: "bold", padding: "0 1rem 0.5rem"}}>Cantidad</div>
           <select
             value={selectedCantidad}
             onChange={e => setSelectedCantidad(e.target.value)}
@@ -298,19 +304,18 @@ function App() {
               <option key={idx} value={cantidad}>{cantidad}ml</option>
             ))}
           </select>
-          <input
-            type="text"
+
+          <div style={{fontSize: "1.2rem", fontWeight: "bold", padding: "0 1rem 0.5rem"}}>Nombre</div>
+          <input type="text"
             placeholder="Escribe un nombre..."
             value={search}
             onChange={(e) => setSearch(e.target.value)}
             style={{
               width: "100%",
               padding: "8px",
-              marginTop: "10px",
               borderRadius: "5px",
               border: "1px solid #ccc",
-            }}
-          />
+            }}/>
         </div>
         {/* Cards */}
         <div className="cards-grid">
@@ -326,6 +331,7 @@ function App() {
                 background: "#fff",
                 minWidth: "200px",
                 maxWidth: "100%",
+                maxHeight: "200px",
               }}
             >
               {item.Imagen && (
